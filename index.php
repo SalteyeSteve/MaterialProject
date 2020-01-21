2bd7a7afa3f291aa4861e0cd3acd2a80f3fcbc1a
Vlad <vlad210694@yahoo.com>
2020-01-21 13:00:20 +0100

    Added stuff

--- index.php
+++ index.php
@@ -1,14 +1,49 @@
-<?php include "bootstrap.php"; ?>
-
-<html lang="en">
+<html lang="nl">
+<meta http-equiv="Content-Type"
+      content="text/html"
+      charset="UTF-8">
 <head>
-    <title>Title</title>
-    <?php include "head.html";?>
+    <title>Xtreem Travel</title>
+    <?php include "head.html"; ?>
 </head>
-<body>
-<footer style="text-align: center">
-    <p>stuff</p>
-    <p>more stuff</p>
-</footer>
-</body>
+    <body>
+        <?php include "nav.html"?>
+        <main>
+            <div class="container">
+                <div class="card img-tab">
+                    <div class="card-img">
+                        <img class="img-tab" alt="Cancunc Mexico" src="img/cancun.jpg">
+                        <span class="card-title">Windsufring</span>
+                    </div>
+                    <div class="card-content">
+                        <h3>Windsufring in Canc&uacute;n M&eacute;xico</h3>
+                        <p>Deze maand is het aantrekkelijkst voor windsurfing in  Canc&uacute;n, M&eacute;xico</p>
+                    </div>
+                </div>
+                <div class="card img-tab">
+                    <div class="card-img">
+                        <img class="img-tab" alt="Himalayas" src="img/himalaya-tab2.jpg">
+                        <span class="card-title">Mountainclimbing</span>
+                    </div>
+                    <div class="card-content">
+                        <h3>Himalayas</h3>
+                        <p>Deze week vertrekt een groep enthousiaste klimmers naar de Himalayas.</p>
+                    </div>
+                </div>
+                <div class="card img-tab">
+                    <div class="card-img">
+                        <img class="img-tab" alt="Costa Rica" src="img/costa-rica-tab.jpg">
+                        <span class="card-title">Snorkelen</span>
+                    </div>
+                    <div class="card-content">
+                        <h3>Costa Rica</h3>
+                        <p>Snorkelen in Costa Rica. Naast de natuurreservaten heeft Costa Rica 's werelds tweede grootste koraalrif</p>
+                    </div>
+                </div>
+
+            </div>
+        </main>
+        <footer>
+        </footer>
+    </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    executeWeatherRequest();
+
     // simple ajax get action
     $('.selector').click(function () {
         let id = $(this).data('id');
@@ -18,43 +18,5 @@ $(document).ready(function () {
             }
         });
     });
-
-    function executeWeatherRequest() {
-        // ajax continuous api call
-        let id = $(this).data('id'),
-            // 10 seconds interval
-            interval = 10000,
-            requestPending = false;
-
-        // requests weather once
-        $.ajax({
-            method: "POST",
-            url: 'ajax/genericAjaxHandler.php',
-            data: {id: id, action: 'getWeather'},
-            success: function (json) {
-               $('body').append(json);
-            }
-        });
-
-        // continuous call
-        // setInterval(function () {
-        //     if (requestPending) return;
-        //
-        //     $.ajax({
-        //         method: "POST",
-        //         url: 'ajax/genericAjaxHandler.php',
-        //         data: {id: id, action: 'getWeather'},
-        //         success: function (json) {
-        //             console.log('Executing weather request');
-        //             console.log(json);
-        //             // let obj = JSON.parse(json);
-        //             requestPending = false;
-        //         }
-        //     });
-        //
-        //     requestPending = true;
-        // }, interval);
-
-    }
 });
 
