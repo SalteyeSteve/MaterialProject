75550732a80accb3e9a13aec37b5ac8514526f60
Vlad <vlad210694@yahoo.com>
2020-01-23 12:00:24 +0100

    Updated structure

--- snorkelen.php
+++ snorkelen.php
@@ -1,3 +1,16 @@
-<div class="container" id="tab4">
-
-</div>
+<html lang="nl">
+<meta http-equiv="Content-Type"
+      content="text/html"
+      charset="UTF-8">
+<head>
+    <title>XtreemTravel</title>
+    <?php include "head.html"; ?>
+</head>
+<body>
+<?php include "nav.html"?>
+<main class="content">
+</main>
+<footer>
+</footer>
+</body>
+</html>
